87ab265ab37ac9d6c76991039ba490305ee01695
Fabian Meyer <rookfighter@develop>
2015-05-07 20:46:12 +0100

    corrected some issues after first trial

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-if [ "$1" == "" ]; then
+if [ "$1" = "" ]
+then
     echo "usage: setup.sh <environment-name>"
     exit 1
 fi
 
-if [ -d "$1" ]; then
+if [ ! -d "$1" ]
+then
     echo "no setup for '$1' found"
     exit 1
 fi
