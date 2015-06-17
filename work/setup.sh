d1f46b592346720f83a736940a3e7c6050e7ee51
Fabian Meyer <fabian@emutex.com>
2015-06-17 16:56:19 +0100

    added more files to work

--- work/setup.sh
+++ work/setup.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
 sudo apt-get update
-sudo apt-get install build-essential cmake git emacs emacs-goodies-el pymacs pyflakes vim screen gitk
+sudo apt-get -y install build-essential git emacs emacs-goodies-el pymacs pyflakes vim screen gitk meld libcurl4-openssl-dev sqlite3 mysql-client mysql-server lua5.1 luarocks libjson0-dev
 sudo apt-get -y upgrade
+
+sudo luarocks install busted
+sudo luarocks install luacheck
+sudo luarocks install luasocket
+sudo luarocks install luaposix
 
 cp .bashrc ~/
 cp .gitconfig ~/
