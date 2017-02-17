f96fc4c162608713b48e8eb04a78943087d8136f
Fabian Meyer <rookfighter@outlook.de>
2017-02-17 15:14:38 +0100

    Update home setup

--- home/setup.sh
+++ home/setup.sh
@@ -1,33 +1,43 @@
 #!/bin/bash -e
 
 sudo apt-get update
-sudo apt-get -y install build-essential cmake git autoconf vim screen gitk skype dropbox python3 python3-pip audacious vlc flashplugin-installer pinta openjdk-8-jdk mono-runtime openvpn network-manager-openvpn-gnome dia easymp3gain-gtk easytag texlive-full texstudio virtualbox keepass2 meld
+sudo apt-get -y install build-essential cmake git autoconf vim screen gitk skype python3 python3-pip audacious vlc flashplugin-installer pinta openjdk-8-jdk mono-complete openvpn network-manager-openvpn-gnome dia easymp3gain-gtk easytag texlive-full texstudio virtualbox keepass2 meld unzip
 sudo apt-get -y upgrade
 
 HOME_DIR="/home/$(whoami)"
-PROGRAMS_DIR="$HOME_DIR/Programs"
+PROGRAMS_DIR="$HOME_DIR/opt"
 DESKTOP_DIR="$HOME_DIR/.local/share/applications"
+
+# URLs
+RAINLENDAR_URL="http://www.rainlendar.net/download/rainlendar2-pro_2.13.1.b147-1_amd64.deb"
+ANKI_URL="http://ankisrs.net/download/mirror/anki-2.0.36.deb"
+ECLIPSE_URL="http://www.mirrorservice.org/sites/download.eclipse.org/eclipseMirror/technology/epp/downloads/release/neon/2/eclipse-jee-neon-2-linux-gtk-x86_64.tar.gz"
+CMAKEED_URL="http://downloads.sourceforge.net/project/cmakeed/CMakeEd-1.1.6.zip"
+UMLET_URL="http://www.umlet.com/umlet_14_2/umlet-standalone-14.2.zip"
+ZEROK_URL="http://zero-k.info/lobby/Chobby.exe"
 
 mkdir -p "$PROGRAMS_DIR"
 
 # Rainlendar
-wget http://www.rainlendar.net/download/rainlendar2-lite_2.12.2.b138-1_amd64.deb -O rainlendar.deb
+wget "$RAINLENDAR_URL" -O rainlendar.deb
 set +e
 sudo dpkg -i rainlendar.deb
 set -e
 rm rainlendar.deb
 sudo apt-get -y install -f
 
+# Seafile
+sudo add-apt-repository ppa:seafile/seafile-client
+sudo apt-get update
+sudo apt-get -y install seafile-gui
+
 # Atom Editor
-wget https://atom.io/download/deb -O atom.deb
-set +e
-sudo dpkg -i atom.deb
-set -e
-rm atom.deb
-sudo apt-get -y install -f
+sudo add-apt-repository ppa:webupd8team/atom
+sudo apt-get update
+sudo apt-get -y install atom
 
 # Anki Flashcards
-wget http://ankisrs.net/download/mirror/anki-2.0.36.deb -O anki.deb
+wget "$ANKI_URL" -O anki.deb
 set +e
 sudo dpkg -i anki.deb
 set -e
@@ -36,27 +46,28 @@ sudo apt-get -y install -f
 
 # Eclipse
 ECLIPSE_ROOT="$PROGRAMS_DIR/eclipse"
-wget "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/neon/R/eclipse-java-neon-R-linux-gtk-x86_64.tar.gz&r=1" -O eclipse.tar.gz
+wget "$ECLIPSE_URL" -O eclipse.tar.gz
 tar xzf eclipse.tar.gz -C "$PROGRAMS_DIR"
 rm eclipse.tar.gz
 
 # Eclipse CMakeEd
-wget http://downloads.sourceforge.net/project/cmakeed/CMakeEd-1.1.6.zip
-unzip CMakeEd-1.1.6.zip
-mv CMakeEd-1.1.6/plugins/* "$ECLIPSE_ROOT/plugins/"
-mv CMakeEd-1.1.6/features/* "$ECLIPSE_ROOT/features/"
-rm -rf CMakeEd-1.1.6.zip CMakeEd-1.1.6
+wget "$CMAKEED_URL" -O CMakeEd.zip
+unzip CMakeEd.zip
+mv "CMakeEd-1.1.6" "CMakeEd"
+mv CMakeEd/plugins/* "$ECLIPSE_ROOT/plugins/"
+mv CMakeEd/features/* "$ECLIPSE_ROOT/features/"
+rm -rf CMakeEd.zip CMakeEd
 
 # Umlet
-wget http://www.umlet.com/umlet_14_2/umlet-standalone-14.2.zip
-unzip umlet-standalone-14.2.zip
-mv Umlet "$PROGRAMS_DIR"
-rm umlet-standalone-14.2.zip
+wget "$UMLET_URL" -O umlet.zip
+unzip umlet.zip
+mv Umlet "$PROGRAMS_DIR/umlet"
+rm umlet.zip
 
 # Zero-K
 mkdir -p "$PROGRAMS_DIR/zero-k"
-wget http://zero-k.info/lobby/Zero-K.exe
-mv Zero-K.exe "$PROGRAMS_DIR/zero-k"
+wget "$ZEROK_URL"
+mv Chobby.exe "$PROGRAMS_DIR/zero-k"
 
 cat .bashrc >> "$HOME_DIR/.bashrc"
 cp .gitconfig "$HOME_DIR"
