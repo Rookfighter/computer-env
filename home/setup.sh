53d0aced160d62013d020ad2c9cbb52b4ad4b313
Fabian Meyer <user8324@posteo.net>
2018-02-21 16:24:24 +0100

    Update setup.sh

--- home/setup.sh
+++ home/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 sudo apt-get update
-sudo apt-get -y install build-essential cmake git autoconf vim screen gitk skype python3 python3-pip audacious vlc flashplugin-installer pinta openjdk-8-jdk mono-complete openvpn network-manager-openvpn-gnome dia easymp3gain-gtk easytag texlive-full texstudio virtualbox keepass2 meld unzip
+sudo apt-get -y install build-essential cmake git autoconf vim screen gitk python3 python3-pip clementine vlc flashplugin-installer pinta openjdk-8-jdk mono-complete openvpn network-manager-openvpn-gnome easymp3gain-gtk easytag texlive-full texstudio virtualbox keepass2 meld unzip
 sudo apt-get -y upgrade
 
 HOME_DIR="/home/$(whoami)"
@@ -11,10 +11,6 @@ DESKTOP_DIR="$HOME_DIR/.local/share/applications"
 # URLs
 RAINLENDAR_URL="http://www.rainlendar.net/download/rainlendar2-pro_2.13.1.b147-1_amd64.deb"
 ANKI_URL="http://ankisrs.net/download/mirror/anki-2.0.36.deb"
-ECLIPSE_URL="http://www.mirrorservice.org/sites/download.eclipse.org/eclipseMirror/technology/epp/downloads/release/neon/2/eclipse-jee-neon-2-linux-gtk-x86_64.tar.gz"
-CMAKEED_URL="http://downloads.sourceforge.net/project/cmakeed/CMakeEd-1.1.6.zip"
-UMLET_URL="http://www.umlet.com/umlet_14_2/umlet-standalone-14.2.zip"
-ZEROK_URL="http://zero-k.info/lobby/Chobby.exe"
 
 mkdir -p "$PROGRAMS_DIR"
 
@@ -43,31 +39,6 @@ sudo dpkg -i anki.deb
 set -e
 rm anki.deb
 sudo apt-get -y install -f
-
-# Eclipse
-ECLIPSE_ROOT="$PROGRAMS_DIR/eclipse"
-wget "$ECLIPSE_URL" -O eclipse.tar.gz
-tar xzf eclipse.tar.gz -C "$PROGRAMS_DIR"
-rm eclipse.tar.gz
-
-# Eclipse CMakeEd
-wget "$CMAKEED_URL" -O CMakeEd.zip
-unzip CMakeEd.zip
-mv "CMakeEd-1.1.6" "CMakeEd"
-mv CMakeEd/plugins/* "$ECLIPSE_ROOT/plugins/"
-mv CMakeEd/features/* "$ECLIPSE_ROOT/features/"
-rm -rf CMakeEd.zip CMakeEd
-
-# Umlet
-wget "$UMLET_URL" -O umlet.zip
-unzip umlet.zip
-mv Umlet "$PROGRAMS_DIR/umlet"
-rm umlet.zip
-
-# Zero-K
-mkdir -p "$PROGRAMS_DIR/zero-k"
-wget "$ZEROK_URL"
-mv Chobby.exe "$PROGRAMS_DIR/zero-k"
 
 cat .bashrc >> "$HOME_DIR/.bashrc"
 cp .gitconfig "$HOME_DIR"
