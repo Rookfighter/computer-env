9fec933b1da88284d580412dbdf7da11a570d8e2
Fabian Meyer <user8324@posteo.net>
2018-04-11 21:03:44 +0200

    Add setup for roblab

---
+++ roblab/setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -e
+
+sudo apt-get update
+sudo apt-get -y install build-essential cmake git autoconf vim screen gitk python3 python3-pip meld unzip
+sudo apt-get -y upgrade
+
+CURR_DIR="$(pwd)"
+HOME_DIR="/home/$(whoami)"
+PROGRAMS_DIR="$HOME_DIR/opt"
+DESKTOP_DIR="$HOME_DIR/.local/share/applications"
+
+# Atom Editor
+git clone "https://github.com/Rookfighter/atom-settings.git" "$HOME_DIR/.atom"
+sudo add-apt-repository ppa:webupd8team/atom
+sudo apt-get update
+sudo apt-get -y install atom
+cd "$HOME_DIR/.atom"
+./package.sh install
+cd "$CURR_DIR"
+
+# ROS
+sudo echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > "/etc/apt/sources.list.d/ros-latest.list"
+sudo apt-key adv --keyserver "hkp://ha.pool.sks-keyservers.net:80" --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+sudo apt-get update
+sudo apt-get install ros-lunar-desktop-full
+
+cat .bashrc >> "$HOME_DIR/.bashrc"
+cp .gitconfig "$HOME_DIR"
+cp .vimrc "$HOME_DIR"
+
--- work/.vimrc
+++
@@ -1,6 +0,0 @@
-syntax enable
-set background=light
-set ai
-set tabstop=4
-set shiftwidth=4
-set expandtab
