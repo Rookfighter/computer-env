000cd3ac3e60cc6b5c1b05f06815b580a6d56c4f
Fabian Meyer <user8324@posteo.net>
2019-12-19 10:32:59 +0100

    Update setup.sh

--- home/setup.sh
+++ home/setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -e
 
 sudo add-apt-repository -y ppa:seafile/seafile-client
-sudo add-apt-repository -y ppa:webupd8team/atom
 sudo apt-get update
 sudo apt-get -y install \
     build-essential cmake git gitk meld autoconf vim screen  \
@@ -10,7 +9,7 @@ sudo apt-get -y install \
     openjdk-8-jdk mono-complete \
     openvpn network-manager-openvpn-gnome bmon \
     texlive-full texstudio unzip \
-    seafile-gui atom 
+    seafile-gui
 sudo apt-get -y upgrade
 
 BASE_DIR=$(cd $(dirname "$1") && pwd)
@@ -20,7 +19,8 @@ DESKTOP_DIR="$HOME_DIR/.local/share/applications"
 
 # URLs
 RAINLENDAR_URL="https://www.rainlendar.net/download/rainlendar2-pro_2.15.1.b163-1_amd64.deb"
-BITWARDEN_URL="https://vault.bitwarden.com/download/?app=desktop&platform=linux&variant=appimage"
+BITWARDEN_URL="https://github.com/bitwarden/desktop/releases/download/v1.16.6/Bitwarden-1.16.6-amd64.deb"
+ATOM_URL="https://github.com/atom/atom/releases/download/v1.42.0/atom-amd64.deb"
 
 if [ ! -d "$HOME_DIR/.atom" ]
 then
@@ -34,11 +34,18 @@ mkdir -p "$PROGRAMS_DIR"
 
 # Rainlendar
 wget "$RAINLENDAR_URL" -O rainlendar.deb
-set +e
-sudo dpkg -i rainlendar.deb
-set -e
+sudo gdebi -n rainlendar.deb
 rm rainlendar.deb
-sudo apt-get -y install -f
+
+# Rainlendar
+wget "$BITWARDEN_URL" -O bitwarden.deb
+sudo gdebi -n bitwarden.deb
+rm bitwarden.deb
+
+# Atom
+wget "$ATOM_URL" -O atom.deb
+sudo gdebi -n atom.deb
+rm atom.deb
 
 cat .bashrc >> "$HOME_DIR/.bashrc"
 cp .gitconfig "$HOME_DIR"
