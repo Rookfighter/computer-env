be4a2e5bb46208eec7c854f9362550c83c21a8f5
Fabian Meyer <rookfighter@posteo.org>
2023-03-09 00:26:07 +0100

    Update setup.sh

--- home/setup.sh
+++ home/setup.sh
@@ -5,7 +5,7 @@ sudo apt-get -y install \
     build-essential cmake git gitk meld autoconf vim screen \
     python3 python3-pip python3-pylint virtualenv \
     vlc easytag pinta \
-    bmon curl wget \
+    bmon curl wget htop \
     texlive-full texstudio unzip
 sudo apt-get -y upgrade
 
@@ -17,9 +17,10 @@ DESKTOP_DIR="$HOME_DIR/.local/share/applications"
 # URLs
 VSCODE_URL="https://az764295.vo.msecnd.net/stable/5235c6bb189b60b01b1f49062f4ffa42384f8c91/code_1.74.0-1670260027_amd64.deb"
 RAINLENDAR_URL="https://www.rainlendar.net/download/2.18.0/rainlendar2-pro_2.18.0.b171-1_amd64.deb"
-BITWARDEN_URL="https://github.com/bitwarden/clients/releases/download/desktop-v2022.11.0/Bitwarden-2022.11.0-x86_64.AppImage"
-ZOTERO_URL="https://download.zotero.org/client/release/6.0.18/Zotero-6.0.18_linux-x86_64.tar.bz2"
-NVIM_URL="https://github.com/neovim/neovim/releases/download/v0.8.1/nvim-linux64.deb"
+BITWARDEN_URL="https://github.com/bitwarden/clients/releases/download/desktop-v2023.2.0/Bitwarden-2023.2.0-amd64.deb"
+ZOTERO_URL="https://download.zotero.org/client/release/6.0.22/Zotero-6.0.22_linux-x86_64.tar.bz2"
+NVIM_URL="https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.deb"
+BUILD2_URL="https://stage.build2.org/0/0.16.0-a.0/build2-install-0.16.0-a.0-stage.sh"
 
 mkdir -p "$PROGRAMS_DIR"
 
@@ -39,10 +40,9 @@ sudo gdebi -n nvim.deb
 rm nvim.deb
 
 # Bitwarden
-BITWARDEN_DIR="$PROGRAMS_DIR/Bitwarden"
-mkdir -p "$BITWARDEN_DIR"
-wget "$BITWARDEN_URL" -O "$BITWARDEN_DIR/Bitwarden"
-cp "$BASE_DIR/Bitwarden.png" "$BITWARDEN_DIR"
+wget "$BITWARDEN_URL" -O Bitwarden.deb
+sudo gdebi -n Bitwarden.deb
+rm Bitwarden.deb
 
 # Zotero
 ZOTERO_DIR="$PROGRAMS_DIR/Zotero"
@@ -66,8 +66,8 @@ cp *.desktop "$DESKTOP_DIR"
 sed -i 's|##PROGRAMS_DIR##|'$PROGRAMS_DIR'|g' "$DESKTOP_DIR/"*.desktop
 
 # Build2
-BUILD2_DIR="$HOME_DIR/build2-build"
+BUILD2_DIR="$HOME_DIR/Downloads/build2-build"
 mkdir -p "$BUILD2_DIR"
 cd "$BUILD2_DIR"
-curl -sSfO https://download.build2.org/0.15.0/build2-install-0.15.0.sh
-sh build2-install-0.15.0.sh --local
+wget "$BUILD2_URL" -O build2-install.sh
+sh build2-install.sh --local
